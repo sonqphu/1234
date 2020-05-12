2d0b2818f166dd964518f0a0a4805b4974660cd5
sonqphu <sonqphu@gmail.com>
2020-05-12 08:26:38 +0700

    123.

--- lib/repositories/AuthenticationRepository.dart
+++ lib/repositories/AuthenticationRepository.dart
@@ -1,5 +1,18 @@
-class AuthenticationRepository{
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:messio/providers/AuthenticationProvider.dart';
+import 'package:messio/providers/BaseProviders.dart';
 
+class AuthenticationRepository {
 
+  BaseAuthenticationProvider authenticationProvider = AuthenticationProvider();
 
+  Future<FirebaseUser> signInWithGoogle() =>
+      authenticationProvider.signInWithGoogle();
+
+  Future<void> signOutUser() => authenticationProvider.signOutUser();
+
+  Future<FirebaseUser> getCurrentUser() =>
+      authenticationProvider.getCurrentUser();
+
+  Future<bool> isLoggedIn() => authenticationProvider.isLoggedIn();
 }
